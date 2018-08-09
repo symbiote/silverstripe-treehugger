51c56e782ab0473d9f0495b60248cee09f5e1b23
Jake B <jake@symbiote.com.au>
2018-08-09 17:39:26 +1000

    fix(SortableMenuManageExtension): Fix bugs with Multisites and add tests

--- code/SortableMenuManageExtension.php
+++ code/SortableMenuManageExtension.php
@@ -10,29 +10,47 @@ class SortableMenuManageExtension extends Extension
         }
         $this->owner->_sortable_menu_manage_getcmsfields_called = true;
 
-        $rootTabSet = $fields->fieldByName('Root');
-        $sortableMenuTab = $rootTabSet->fieldByName('SortableMenu');
-        if (!$sortableMenuTab) {
-            $sortableMenuTab = TabSet::create('SortableMenu', 'Menus');
-            $rootTabSet->push($sortableMenuTab);
+        // Base class
+        $basePageClass = '';
+        if (SiteTree::has_extension('SortableMenu')) {
+            // NOTE(Jake): 2018-08-09
+            //
+            // An SS 3.X project applied to the SiteTree so we need
+            // this for backwards compatibility.
+            //
+            $basePageClass = 'SiteTree';
+        } else if (Page::has_extension('SortableMenu')) {
+            $basePageClass = 'Page';
         }
-        $sortableMenuTab->setTitle('Menus');
-        if (!$sortableMenuTab instanceof TabSet) {
-            $badClass = get_class($sortableMenuTab);
-            throw new SortableMenuException('Sortable Menu must be a "TabSet", not "'.$badClass.'"');
-        }
-        $sortableMenuTab = $fields->findOrMakeTab('Root.SortableMenu', 'Menus');
-        $menus = singleton('SortableMenu')->getSortableMenuConfiguration();
-        foreach ($menus as $fieldName => $extraInfo) {
-            $fieldTitle = $extraInfo['Title'];
-            // NOTE(Jake): Check if Tab has already been created by user-code / outside of the module
-            $menuTab = $sortableMenuTab->fieldByName($fieldName);
-            if (!($menuTab instanceof Tab)) {
-                $menuTab = Tab::create($fieldName, $fieldTitle);
-                $sortableMenuTab->push($menuTab);
+
+        // If one of the expected classes has the extension, then generate
+        // the fields.
+        if ($basePageClass !== '') {
+            // Setup fields
+            $rootTabSet = $fields->fieldByName('Root');
+            $sortableMenuTab = $rootTabSet->fieldByName('SortableMenu');
+            if (!$sortableMenuTab) {
+                $sortableMenuTab = TabSet::create('SortableMenu', 'Menus');
+                $rootTabSet->push($sortableMenuTab);
             }
-            $menuTab->setTitle($fieldTitle);
-            $menuTab->push($this->owner->createMenuGridField('SiteTree', $fieldName, $fieldTitle, $extraInfo['Sort']));
+            $sortableMenuTab->setTitle('Menus');
+            if (!$sortableMenuTab instanceof TabSet) {
+                $badClass = get_class($sortableMenuTab);
+                throw new SortableMenuException('Sortable Menu must be a "TabSet", not "'.$badClass.'"');
+            }
+            $sortableMenuTab = $fields->findOrMakeTab('Root.SortableMenu', 'Menus');
+            $menus = singleton('SortableMenu')->getSortableMenuConfiguration();
+            foreach ($menus as $fieldName => $extraInfo) {
+                $fieldTitle = $extraInfo['Title'];
+                // NOTE(Jake): Check if Tab has already been created by user-code / outside of the module
+                $menuTab = $sortableMenuTab->fieldByName($fieldName);
+                if (!($menuTab instanceof Tab)) {
+                    $menuTab = Tab::create($fieldName, $fieldTitle);
+                    $sortableMenuTab->push($menuTab);
+                }
+                $menuTab->setTitle($fieldTitle);
+                $menuTab->push($this->owner->createMenuGridField($basePageClass, $fieldName, $fieldTitle, $extraInfo['Sort']));
+            }
         }
     }
 
@@ -46,6 +64,12 @@ class SortableMenuManageExtension extends Extension
 
     public function createMenuGridField($class, $fieldName, $fieldTitle, $sortFieldName)
     {
+        if (!class_exists($class)) {
+            throw new SortableMenuException($class.' does not exist.');
+        }
+        if (!$class::has_extension('SortableMenu')) {
+            throw new SortableMenuException($class.' does not have SortableMenu extension applied.');
+        }
         $record = singleton($class);
         $list = $record->SortableMenu($fieldName);
 
@@ -58,7 +82,9 @@ class SortableMenuManageExtension extends Extension
         $config->removeComponentsByType('GridFieldDeleteAction');
         $config->removeComponentsByType('GridFieldFilterHeader');
         $config->addComponent(Injector::inst()->createWithArgs('GridFieldAddSortableMenuItem', array($fieldName)));
-        $config->addComponent(GridFieldOrderableRows::create($sortFieldName));
+        if (class_exists('GridFieldOrderableRows')) {
+            $config->addComponent(GridFieldOrderableRows::create($sortFieldName));
+        }
         $dataColumns = $config->getComponentByType('GridFieldDataColumns');
         if ($dataColumns) {
             $dataColumns->setDisplayFields(array(
---
+++ tests/SortableMenuMultisitesTest.php
@@ -0,0 +1,63 @@
+<?php
+
+class SortableMenuMultisitesTest extends FunctionalTest
+{
+    protected static $use_draft_site = true;
+
+    protected $usesDatabase = true;
+
+    public function setUp()
+    {
+        // NOTE(Jake): 2018-08-09
+        //
+        // If we can't find "Site" class, skip all tests.
+        //
+        if (!class_exists('Site')) {
+            $this->skipTest = true;
+            parent::setUp();
+            return;
+        }
+
+        Config::inst()->update('SortableMenu', 'menus', array(
+            'ShowInFooter' => array(
+                'Title' => 'Footer',
+            ),
+            'ShowInSidebar' => array(
+                'Title' => 'Sidebar',
+            ),
+        ));
+        // NOTE(Jake): 2018-08-09
+        //
+        // The core `$requiredExtensions` functionality isn't working here in SS 3.X.
+        // I suspect its not flushing the YML or something?
+        //
+        Page::add_extension('SortableMenu');
+        parent::setUp();
+    }
+
+    public function testLoadEditingPageWithNoData()
+    {
+        $this->logInWithPermission('ADMIN');
+
+        //
+        $site = new Site();
+        $site->Title = 'Test';
+        $site->write();
+        $site->doPublish();
+
+        $pageID = $site->ID;
+        $response = $this->get('admin/pages/edit/show/' . $pageID);
+
+        // Check that the GridFields have been created in the CMS
+        $this->assertContains('<input type="hidden" name="ShowInFooter[GridState]" ', $response->getBody());
+        $this->assertContains('<input type="hidden" name="ShowInSidebar[GridState]" ', $response->getBody());
+    }
+
+    /**
+     * Taken from "framework\tests\view\SSViewerTest.php"
+     */
+    protected function assertEqualIgnoringWhitespace($a, $b, $message = '')
+    {
+        $this->assertEquals(preg_replace('/\s/', '', $a), preg_replace('/\s/', '', $b), $message);
+    }
+}

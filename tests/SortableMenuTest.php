38812fe8b4b586c97ae897c9204df62dfbf46867
Jake B <jake@symbiote.com.au>
2018-08-09 14:20:58 +1000

    fix(SortableMenuTest): fix stub test

--- tests/SortableMenuTest.php
+++ tests/SortableMenuTest.php
@@ -2,5 +2,13 @@
 
 class SortableMenuTest extends SapphireTest
 {
+    protected $usesDatabase = false;
 
+    public function testRenderingMenus()
+    {
+        $this->markTestSkipped(
+            'Skipping '. get_class($this).'::'.__FUNCTION__.'() as this is a stub for now.'
+        );
+        $this->skipTest = true;
+    }
 }

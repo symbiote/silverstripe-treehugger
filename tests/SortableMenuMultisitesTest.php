b1609d8ef3bf8d4f07aff0bd414462ffcd2b702b
Jake B <jake@symbiote.com.au>
2018-08-10 14:58:18 +1000

    test

--- tests/SortableMenuMultisitesTest.php
+++ tests/SortableMenuMultisitesTest.php
@@ -6,6 +6,11 @@
 // When Multisites is installed, this is the only test that can pass as
 // the others get validation errors due to how Multisites works.
 //
+// At the time of writing, this test is only executed in TravisCI if
+// the environment var `MULTISITES_VERSION` is set.
+//
+// Refer to .travis.yml and see when this is executed.
+//
 class SortableMenuMultisitesTest extends FunctionalTest
 {
     protected static $use_draft_site = true;
@@ -16,7 +21,8 @@ class SortableMenuMultisitesTest extends FunctionalTest
     {
         // NOTE(Jake): 2018-08-09
         //
-        // If we can't find "Site" class, skip all tests.
+        // If we can't find "Site" class, skip all tests
+        // in this class.
         //
         if (!class_exists('Site')) {
             $this->skipTest = true;
